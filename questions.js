92cfc8810fbc15c97ab121e8b5e4d2dc29cdee1e
Anshuman7540 <87190273+Anshuman7540@users.noreply.github.com>
2021-07-31 18:40:01 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The CSS property used to control the element's font-size is",
+    answer: "font-size",
+    options: [
+      "text-style",
+      "text-size",
+      "font-size",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Is HTML a scripting language",
+    answer: "No",
+    options: [
+      "Yes",
+      "No",
+      "Maybe",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The latest HTML standard is",
+    answer: "HTML 5",
+    options: [
+      "XML",
+      "HTML 5",
+      "SGML",
+      "HTML 3"
+    ]
+  },
+    {
+    numb: 5,
+    question: "HTML was developed by",
+    answer: "Tim Berners-Lee",
+    options: [
+      "Tim Bruce",
+      "Tim Berners-Lee",
+      "Hakon Wium Lie",
+      "Guido van Rossum"
+    ]
+  },
+   {
+    numb: 6,
+    question: "Which tool can you use to ensure code quality?",
+    answer: "ESLint",
+    options: [
+      "Angular",
+      "jQuery",
+      "RequireJS",
+      "ESLint"
+    ]
+  },
+  {
+    numb: 7,
+    question: "HTML uses",
+    answer: "Pre-specified Tags",
+    options: [
+      "User Defined Tags",
+      "Pre-specified Tags",
+      "Tags Only For Linking",
+      "Fixed Tags Defined By The Language"
+    ]
+  },
+  {
+    numb: 8,
+    question: "The CSS property used to specify whether a text is written in horizontal or vertical direction",
+    answer: "writing-mode",
+    options: [
+      "writing-mode",
+      "text-indent",
+      "word-break",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The HTML Geolocation API is used to get the",
+    answer: "geographical position",
+    options: [
+      "drag and drop position",
+      "geographical position",
+      "create graphics",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following CSS property is used to add shadows to the text?",
+    answer: "text-shadow",
+    options: [
+      "text-stroke",
+      "text-overflow",
+      "text-shadow",
+      "text-decoration"
+    ]
+  },
+];
